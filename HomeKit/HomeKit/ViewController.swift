70b182bff897d4d6a85ba8bdde7c57db2f334da2
hgxl <henrigil@MacBook-Pro-de-Henri.local>
2018-10-28 09:01:57 +0100

    First

---
+++ HomeKit/HomeKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeKit
+//
+//  Created by Henri Gil on 28/10/2018.
+//  Copyright © 2018 Henri Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
